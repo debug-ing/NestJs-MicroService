f9defc95d3e547113e097cbc920b8267d7ba3717
M4HDI <mahdicsharp0098@gmail.com>
2021-10-31 15:43:10 +0330

    add profix 'api'

--- getway/src/main.ts
+++ getway/src/main.ts
@@ -5,6 +5,7 @@ import {ValidationPipe} from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
